35a504055a0931155e88165735a2e0b33651059a
Christopher <chcodu@gmail.com>
2021-08-02 21:05:01 -0600

    Se agregan los estilos y la primera versión de los botones

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
-ReactDom.render(<h1>Hola mundo</h1>,
+// TODO: Se utiliza el componente principal
+ReactDom.render(<App />,
                     document.getElementById('root'));
